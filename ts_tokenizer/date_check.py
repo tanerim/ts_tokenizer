435fd92afa44e1929a3b5a682379a0d5c51f6303
tanerim <tanersezerr@gmail.com>
2024-06-01 13:34:45 +0300

    Decorators

--- ts_tokenizer/date_check.py
+++ ts_tokenizer/date_check.py
@@ -6,7 +6,7 @@ class DateValidator:
         pass
 
     @classmethod
-    def is_valid_date_dd_mm_yyyy(cls, date_str):
+    def is_valid_date_dd_mm_yyyy(cls, date_str: str) -> bool:
         match = re.match(r"\b(0?[1-9]|[1-2][0-9]|3[0-1])([-/.:])(0?[1-9]|1[0-2])\2(\d{2,4})\b", date_str)
         if not match:
             return False
@@ -15,7 +15,7 @@ class DateValidator:
         return cls._is_valid_day_month_year(day, month, year)
 
     @classmethod
-    def is_valid_date_mm_dd_yyyy(cls, date_str):
+    def is_valid_date_mm_dd_yyyy(cls, date_str: str) -> bool:
         match = re.match(r"\b(0?[1-9]|1[0-2])([-/.:])(0?[1-9]|[1-2][0-9]|3[0-1])\2(\d{2,4})\b", date_str)
         if not match:
             return False
@@ -24,7 +24,7 @@ class DateValidator:
         return cls._is_valid_day_month_year(day, month, year)
 
     @classmethod
-    def _is_valid_day_month_year(cls, day, month, year):
+    def _is_valid_day_month_year(cls, day: int, month: int, year: int) -> bool:
         if month in [1, 3, 5, 7, 8, 10, 12]:
             max_day = 31
         elif month in [4, 6, 9, 11]:
@@ -41,5 +41,5 @@ class DateValidator:
 
 class DateCheck:
     @staticmethod
-    def is_date(date_str):
+    def is_date(date_str: str) -> bool:
         return DateValidator.is_valid_date_mm_dd_yyyy(date_str) or DateValidator.is_valid_date_dd_mm_yyyy(date_str)
