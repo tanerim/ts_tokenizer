6daefbca1dd7869579a7ceb82b3d295e844a2600
tanerim <tanersezerr@gmail.com>
2024-06-11 02:58:20 +0300

    Decorators

--- ts_tokenizer/inner_punc.py
+++ ts_tokenizer/inner_punc.py
@@ -1,5 +1,6 @@
 import re
 import string
+from typing import Union, Optional
 num_suffix = ["ıncı", "inci", "üncü", "uncu"]
 
 
@@ -9,22 +10,27 @@ class InnerPuncParser:
         self.pattern = r'^(?P<initial>[%s]*)(?P<word>.*?)(?P<final>[%s]*)$' % (self.puncs, self.puncs)
 
     @classmethod
-    def ord_number(cls, word):
+    def ord_number(cls, word: str) -> Optional[Union[bool, tuple]]:
         if "'" in word:
             word_parts = word.split("'")
             if word_parts[0].isdigit() and word_parts[1] in num_suffix:
                 return word, "Ord_Number"
 
+
+    # More methods required following outputs of K-Means output
+    # such as ev,araba is OK
+    # but samples like fill-in-the blank questions like Civanın özkütlesi ______________ 'dir. is not
+
     @classmethod
-    def tokenize_Inner_Punc(cls, word):
-        if word.count("'") == 1 and not word.startswith("'") and not word.endswith("'"):
-            return word
-        # Find the index of each punctuation character in the word
-        index_list = [i for i, char in enumerate(word) if char in string.punctuation]
+    def tokenize_Inner_Punc(cls, word: str) -> str:
         # Initialize an empty string to store the result
         result = ""
         # Initialize the start index for slicing
         start = 0
+        if word.count("'") == 1 and not word.startswith("'") and not word.endswith("'"):
+            return word
+        # Find the index of each punctuation character in the word
+        index_list = [i for i, char in enumerate(word) if char in string.punctuation]
         for index in index_list:
             # Append the substring from start to index, and then append a newline
             result += word[start:index] + '\n'
@@ -34,4 +40,4 @@ class InnerPuncParser:
             start = index + 1
         # Append the remaining substring after the last index
         result += word[start:]
-        return result.strip()
+        return result.strip().replace("\n\n", "\n")
