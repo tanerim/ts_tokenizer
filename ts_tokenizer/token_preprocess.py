dda6a30f6a2fb3052693039baed179b0e22dc55a
tanerim <tanersezerr@gmail.com>
2024-10-01 00:43:13 +0300

    Updates

--- ts_tokenizer/token_preprocess.py
+++ ts_tokenizer/token_preprocess.py
@@ -34,12 +34,37 @@ REGEX_PATTERNS = {
     "three_or_more": r'([' + re.escape(string.punctuation) + r'])\1{2,}',
     "num_char_sequence": r'\d+[\w\s]*',
     "roman_number": r'^(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))\.?$',
-    "currency": rf"([{LocalData.currency_symbols()}]\d{{1,3}}(?:[.,]\d{{3}})*([.,]\d+)?)'.format(symbols=re.escape(''.join(LocalData.currency_symbols())))",
+    "apostrophed": r"\b\w+'[a-zA-Z]+\b",
+    "currency": rf"(?:[{re.escape(''.join(LocalData.currency_symbols()))}]\d{{1,3}}(?:[.,]\d{{3}})*([.,]\d+)?|\d{{1,3}}(?:[.,]\d{{3}})*([.,]\d+)?[{re.escape(''.join(LocalData.currency_symbols()))}])"
 }
+
+exception_list = ["(!)", "...", "[...]"]
 
 
 def check_regex(word, pattern):
-    return word if re.search(REGEX_PATTERNS[pattern], word) else None
+    return word if re.search(REGEX_PATTERNS[pattern], CharFix.fix(word)) else None
+
+
+def punc_count(word: str) -> int:
+    return sum(1 for char in word if char in puncs)
+
+
+def punc_pos(word: str) -> list:
+    return [i for i, char in enumerate(word) if char in puncs]
+
+
+def apply_charfix(func):
+    def wrapper(word, *args, **kwargs):
+        fixed_word = CharFix.fix(word)
+        return func(fixed_word, *args, **kwargs)  # Return the result
+    return wrapper
+
+
+def tr_lowercase(func):
+    def wrapper(word, *args, **kwargs):
+        turkish_lowercase = CharFix.tr_lowercase(word)
+        return func(turkish_lowercase, *args, **kwargs)  # Return the wrapped function
+    return wrapper
 
 
 class TokenPreProcess:
@@ -51,34 +76,34 @@ class TokenPreProcess:
     # These functions get the input token, checks against to regexs defined above and
     # return word, tag as tuple
     @staticmethod
+    @apply_charfix
     def is_xml(word: str) -> tuple:
         result = check_regex(word, "xml_tag")
         return (result, "XML_Tag") if result else None
 
     @staticmethod
+    @apply_charfix
     def is_mention(word: str) -> tuple:
         p_count = PuncMatcher.punc_count(word)
         result = check_regex(word, "mention") if p_count == 1 else None
         return (result, "Mention") if result else None
 
     @staticmethod
+    @apply_charfix
     def is_hashtag(word: str) -> tuple:
         p_count = PuncMatcher.punc_count(word)
         result = check_regex(word, "hashtag") if p_count == 1 else None
         return (result, "Hashtag") if result else None
 
     @staticmethod
-    def is_num_char_sequence(word: str) -> tuple:
-        result = check_regex(word, "num_char_sequence")
-        return (result, "Num_Char_Sequence") if result else None
-
-    @staticmethod
+    @apply_charfix
     def is_in_quotes(word: str) -> tuple:
         p_count = PuncMatcher.punc_count(word)
         result = check_regex(word, "in_quotes") if p_count <= 2 else None
         return (result, "In_Quotes") if result else None
 
     @staticmethod
+    @apply_charfix
     def is_in_parenthesis(word: str) -> tuple:
         p_count = PuncMatcher.punc_count(word)
         result = check_regex(word, "in_parenthesis") if p_count <= 2 else None
@@ -87,7 +112,7 @@ class TokenPreProcess:
     @staticmethod
     def is_date_range(word: str) -> tuple:
         p_count = PuncMatcher.punc_count(word)
-        result = check_regex(word, "date_range") if p_count <= 1 else None
+        result = check_regex(word, "date_range") if p_count == 1 and PuncMatcher.find_punctuation(word) == "-" else None
         return (result, "Date_Range") if result else None
 
     @staticmethod
@@ -96,83 +121,93 @@ class TokenPreProcess:
         return (result, "Hour") if result else None
 
     @staticmethod
-    def is_date(word):
+    def is_date(word: str) -> tuple:
         result = DateCheck.is_date(word)
-        return (result, "Date") if result else None
+        return (word, "Date") if result else None
 
     @staticmethod
-    def is_percentage_numbers(word):
+    def is_percentage_numbers(word: str) -> tuple:
         p_count = PuncMatcher.punc_count(word)
         result = check_regex(word, "percentage_numbers") if p_count == 1 else None
         return (result, "Percentage_Numbers") if result else None
 
     @staticmethod
-    def is_percentage_numbers_chars(word):
+    @apply_charfix
+    def is_percentage_numbers_chars(word: str) -> tuple:
         result = check_regex(word, "percentage_numbers_chars")
         return (result, "Percentage_Numbers_Chars") if result else None
 
     @staticmethod
-    def is_roman_number(word):
+    def is_roman_number(word: str) -> tuple:
         result = check_regex(word, "roman_number")
         return (result, "Roman_Number") if result else None
 
     @staticmethod
-    def is_email_punc(word):
+    def is_email_punc(word: str) -> tuple:
         result = check_regex(word, "email_punc")
         return (result, "Email_Punc") if result else None
 
     @staticmethod
-    def is_email(word):
+    def is_email(word: str) -> tuple:
         result = check_regex(word, "email")
         return (result, "Email") if result else None
 
     @staticmethod
-    def is_prefix_url(word):
+    def is_prefix_url(word: str) -> tuple:
         if any(dne in word for dne in LocalData.domains()):
             result = check_regex(word, "prefix_url")
             return (result, "Prefix_URL") if result else None
 
     @staticmethod
-    def is_non_prefix_url(word):
+    def is_non_prefix_url(word: str) -> tuple:
         if any(dne in word for dne in LocalData.domains()):
             result = check_regex(word, "Non_Prefix_URL")
             return (result, "Non_Prefix_URL") if result else None
 
     @staticmethod
-    def is_copyright(word):
+    def is_copyright(word: str) -> tuple:
         result = check_regex(word, "copyright")
         return (result, "Copyright") if result else None
 
     @staticmethod
-    def is_registered(word):
+    def is_registered(word: str) -> tuple:
         result = check_regex(word, "registered")
         return (result, "Registered") if result else None
 
     @staticmethod
-    def is_currency(word):
+    def is_currency(word: str) -> tuple:
         result = check_regex(word, "currency")
         return (result, "Currency") if result else None
 
+    @staticmethod
+    @apply_charfix
+    def is_num_char_sequence(word: str) -> tuple:
+        result = check_regex(word, "num_char_sequence")
+        return (result, "Num_Char_Sequence") if result else None
+
     # Lexicon Based Tokens
     @staticmethod
-    def is_in_lexicon(word):
-        word_fixed = CharFix.fix(word)
-        if CharFix.tr_lowercase(word_fixed) in LocalData.word_list():
-            return word_fixed, "Lexicon"
-        return None
+    @apply_charfix
+    @tr_lowercase
+    def is_in_lexicon(word: str) -> tuple:
+        if word in LocalData.word_list():
+            return word, "Valid_Word"
 
     @staticmethod
-    def is_abbr(word):
+    @apply_charfix
+    def is_abbr(word: str) -> tuple:
         return (word, "Abbr") if word in LocalData.abbrs() else None
 
     @staticmethod
-    def is_in_exceptions(word):
-        word_fixed = CharFix.fix(word)
-        if CharFix.tr_lowercase(word_fixed) in LocalData.exception_words():
-            return word_fixed, "Exception"
-        return None
+    @apply_charfix
+    @tr_lowercase
+    def is_in_exceptions(word: str) -> tuple:
+        if CharFix.tr_lowercase(word) in LocalData.exception_words():
+            return word, "Exception"
 
     @staticmethod
+    @apply_charfix
+    @tr_lowercase
     def is_in_eng_words(word):
         word_fixed = CharFix.fix(word)
         if CharFix.tr_lowercase(word_fixed) in LocalData.eng_word_list():
@@ -209,12 +244,71 @@ class TokenPreProcess:
         return (word, "Number") if word.isdigit() else None
 
     @staticmethod
-    def is_punc(word):
-        exception_list = ["(!)", "...", "[...]"]
-        if TokenPreProcess.is_currency(word):
-            return word, "Currency"
-        if any(word.startswith(exc) and word.endswith(tuple(puncs)) and len(word) > len(exc) for exc in exception_list):
+    @apply_charfix
+    @tr_lowercase
+    def is_fsp(word: str) -> tuple:
+        # Check if the word ends with exactly one punctuation mark and has no other punctuation marks
+        if len(word) > 1 and word[-1] in puncs and all(char not in puncs for char in word[:-1]):
             return word, "FSP"
+
+    @staticmethod
+    @apply_charfix
+    @tr_lowercase
+    def is_isp(word: str) -> tuple:
+        if len(word) > 1 and word[0] in puncs and all(char not in puncs for char in word[1:]):
+            return word, "ISP"
+
+    @staticmethod
+    @apply_charfix
+    @tr_lowercase
+    def is_mssp(word: str) -> tuple:
+        if len(word) > 2 and word[0] in puncs and word[-1] in puncs and all(char not in puncs for char in word[1:-1]):
+            return word, "MSSP"
+
+    @staticmethod
+    @apply_charfix
+    @tr_lowercase
+    def is_msp(word: str) -> tuple:
+        if len(word) > 2 and word not in exception_list:
+            start_punc_count = 0
+            for char in word:
+                if char in puncs:
+                    start_punc_count += 1
+                else:
+                    break
+
+            end_punc_count = 0
+            for char in word[::-1]:
+                if char in puncs:
+                    end_punc_count += 1
+                else:
+                    break
+
+            if start_punc_count >= 1 and end_punc_count >= 1 and all(
+                    char not in puncs for char in word[start_punc_count: -end_punc_count]):
+                return word, "MSP"
+
+    @staticmethod
+    @apply_charfix
+    @tr_lowercase
+    def is_midp(word: str) -> tuple:
+        if len(word) > 2:
+            if word[0] not in puncs and word[-1] not in puncs:
+                if any(char in puncs for char in word[1:-1]):
+                    return word, "MIDP"
+
+    @staticmethod
+    @apply_charfix
+    @tr_lowercase
+    def is_apostrophed(word: str) -> tuple:
+        result = check_regex(word, "apostrophed")
+        if result:
+            punc_positions = punc_pos(word)
+            if punc_count(word) == 1 and punc_positions and "'" in word:
+                return word, "Apostrophed"
+
+    @staticmethod
+    def is_punc(word):
         if word in exception_list:
             return word, "Punc"
         return (word, "Punc") if all(char in puncs for char in word) else None
@@ -231,6 +325,7 @@ class TokenPreProcess:
         return None
 
     @staticmethod
+    @apply_charfix
     def is_three_or_more(word):
         exceptions = ["...", "!!!"]
         if word in exceptions:
--- ts_tokenizer/token_processor.py
+++ ts_tokenizer/token_processor.py
@@ -1,14 +1,16 @@
 from ts_tokenizer.token_preprocess import TokenPreProcess
-from ts_tokenizer.punctuation_process import PuncTagCheck
 
 
 class TokenProcessor:
 
     def __init__(self):
         pass
+
     @staticmethod
     def process_token(token):
         check_methods = [
+            # First Check for SGML tags |
+            TokenPreProcess.is_xml,
             # Lexicon Based Tokens
             TokenPreProcess.is_in_lexicon,
             TokenPreProcess.is_abbr,
@@ -17,42 +19,55 @@ class TokenProcessor:
             TokenPreProcess.is_smiley,
             TokenPreProcess.is_emoticon,
             TokenPreProcess.is_number,
-            TokenPreProcess.is_punc,
+
+            # Specific Cases for Punctuation Use
+            TokenPreProcess.is_in_quotes,
+            TokenPreProcess.is_in_parenthesis,
+            TokenPreProcess.is_underscored,
+            TokenPreProcess.is_email,
+            TokenPreProcess.is_prefix_url,
+            TokenPreProcess.is_non_prefix_url,
+            TokenPreProcess.is_multiple_smiley_in,
+            TokenPreProcess.is_multiple_smiley,
+            TokenPreProcess.is_date_range,
+            TokenPreProcess.is_date,
+            TokenPreProcess.is_hour,
 
             # Basic Regex Tokens
-            TokenPreProcess.is_xml,
             TokenPreProcess.is_mention,
             TokenPreProcess.is_hashtag,
             TokenPreProcess.is_copyright,
             TokenPreProcess.is_registered,
             TokenPreProcess.is_currency,
-            TokenPreProcess.is_in_quotes,
-            TokenPreProcess.is_in_parenthesis,
-            TokenPreProcess.is_num_char_sequence,
-            TokenPreProcess.is_date_range,
-            TokenPreProcess.is_date,
-            TokenPreProcess.is_hour,
+
+            # Various Status for Punctuation
+            TokenPreProcess.is_apostrophed,
+            TokenPreProcess.is_fsp,
+            TokenPreProcess.is_isp,
+            TokenPreProcess.is_mssp,
+            TokenPreProcess.is_msp,
+            TokenPreProcess.is_midp,
+
+
+
             TokenPreProcess.is_percentage_numbers_chars,
             TokenPreProcess.is_percentage_numbers,
             TokenPreProcess.is_roman_number,
             TokenPreProcess.is_email_punc,
-            TokenPreProcess.is_email,
-            TokenPreProcess.is_prefix_url,
-            TokenPreProcess.is_non_prefix_url,
-            TokenPreProcess.is_multiple_smiley,
-            TokenPreProcess.is_multiple_smiley_in,
+
+
             TokenPreProcess.is_multiple_emoticon,
-            TokenPreProcess.is_underscored,
             TokenPreProcess.is_three_or_more,
             TokenPreProcess.is_inner_char,
+            TokenPreProcess.is_punc,
             TokenPreProcess.is_non_latin,
-            TokenPreProcess.is_one_char_fixable
+            TokenPreProcess.is_one_char_fixable,
+            #TokenPreProcess.is_num_char_sequence
         ]
 
         for check in check_methods:
             result = check(token)
             if result:
                 return result
-            else:
-                return PuncTagCheck.punc_tag_check(token)
+
         return token, "OOV"  # Default return if no checks match
