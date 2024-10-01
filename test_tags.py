769fea09c3ec040253729b5b3011c634265b46f6
tanerim <tanersezerr@gmail.com>
2024-10-01 16:14:33 +0300

    updates

---
+++ test_tags.py
@@ -0,0 +1,15 @@
+from ts_tokenizer.token_preprocess import TokenPreProcess
+from ts_tokenizer.token_processor import TokenProcessor
+import sys
+
+f = open(sys.argv[1]).read().split('\n')
+
+tokens = []
+for line in f:
+    if TokenPreProcess.is_xml(line):
+        print("\t".join(TokenPreProcess.is_xml(line)))
+        continue
+    for word in line.split(' '):
+        token = TokenProcessor.process_token(word)
+        # print(f"{token[0]}\t{token[1]}")
+        print("\t".join(token))
