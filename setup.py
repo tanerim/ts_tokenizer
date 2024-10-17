87693051ab438b16e129733e68b3a6ddc86113a9
tanerim <tanersezerr@gmail.com>
2024-10-17 16:45:54 +0300

    Verison 0.1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ts_tokenizer',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     package_data={
     'ts_tokenizer': ['data/*.txt'],
@@ -17,10 +17,14 @@ setup(
     },
     author='Taner Sezer',
     author_email='tanersezerr@gmail.com',
-    description='TS Tokenizer is a rule-based tokenizer'
-                'specifically designed for processing Turkish text.'
-                'It provides functionalities to split text into tokens'
-                'following the grammatical and linguistic rules of the Turkish language.',
+    description='TS Tokenizer is a hybrid tokenizer designed for Turkish text.'
+                ' It uses a hybrid (lexicon-based and rule-based) approach to split text into tokens.'
+                ' The tokenizer leverages regular expressions to handle non-standard text elements like dates, percentages, URLs, and punctuation marks.'
+                'Key Features:'
+                ' Hybrid Approach: Uses a hybrid (lexicon-based and rule-based approach) for tokenization.'
+                ' Handling of Special Tokens: Recognizes special tokens like mentions, hashtags, emails, URLs, numbers, smiley, emoticons, etc..'
+                ' Highly Configurable: Provides multiple output formats to suit different NLP processing needs,'
+                ' including plain tokens, tagged tokens, and token-tag pairs in list or line formats.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/tanerim/ts_tokenizer',
