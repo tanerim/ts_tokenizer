776552b430bfadbfb22f8835983b034f88071354
tanerim <tanersezerr@gmail.com>
2024-11-26 12:48:35 +0300

    CheckPoint for 0.1.13

--- reg_try.py
+++ reg_try.py
@@ -9,3 +9,14 @@ from ts_tokenizer.token_handler import TokenProcessor, TokenPreProcess
 word = "Mu¬Sun"
 
 print(TokenPreProcess.is_one_char_fixable(word))
+
+
+from ts_tokenizer.token_handler import TokenPreProcess
+
+word = "ParÃ§a"
+
+print(TokenProcessor.process_token(word))
+
+from ts_tokenizer.tokenizer import tokenize
+
+print(tokenize(word, output="list", return_format="tagged"))
