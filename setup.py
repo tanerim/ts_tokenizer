38503c81d041b37d7b4b374764d3aa6b5405fdfd
tanerim <tanersezerr@gmail.com>
2024-11-28 15:58:43 +0300

    CheckPoint for 0.1.13

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='ts_tokenizer',
     version='0.1.13',
     packages=find_packages(),
+    include_package_data=True,
     package_data={
     'ts_tokenizer': ['data/*.txt'],
     },
@@ -21,15 +22,24 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/tanerim/ts_tokenizer',
+    project_urls={
+        'Bug Tracker': 'https://github.com/tanerim/ts_tokenizer/issues',
+        'Documentation': 'https://github.com/tanerim/ts_tokenizer#readme',
+        'Source Code': 'https://github.com/tanerim/ts_tokenizer',
+    },
     classifiers=[
+        'Development Status :: 1 - Beta',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
         "Natural Language :: Turkish",
+        "Topic :: Tokenization :: Linguistics",
         'Operating System :: OS Independent',
     ],
+    keywords='tokenizer turkish nlp text-processing',
+    license='MIT',
     python_requires='>=3.9',
     repository='https://github.com/tanerim/ts_tokenizer'
 )
