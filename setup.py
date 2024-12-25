3c9d8e9823f59429e0726bc57298833f24a5b5f5
tanerim <tanersezerr@gmail.com>
2024-12-25 12:04:21 +0300

    CheckPoint for 0.1.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ts_tokenizer',
-    version='0.1.17',
+    version='0.1.18',
     packages=find_packages(),
     include_package_data=True,
     package_data={
