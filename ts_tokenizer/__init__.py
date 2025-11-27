0d964f6d6572399986ea9e4ec5512f302d77cbc9
tanerim <tanersezerr@gmail.com>
2025-11-27 14:42:07 +0300

    Minor fixes for 0.1.21

--- ts_tokenizer/__init__.py
+++ ts_tokenizer/__init__.py
@@ -1,1 +1,11 @@
-__version__ = '0.1.20'
+__version__ = '0.1.21'
+
+# Public API exports
+from .tokenizer import tokenize, TSTokenizer
+from .char_fix import CharFix
+
+__all__ = [
+    "tokenize",
+    "TSTokenizer",
+    "CharFix",
+]
